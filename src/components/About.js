f3cca35de4fae8fea85fa311d8a6cc75d51f73ad
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-06-13 11:35:11 -0400

    All tests passing

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(prop) {
+  if (prop.bio) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{prop.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={prop.github} linkedin={prop.linkedin}/>
+      </div>
+    );
+  }else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={prop.github} linkedin={prop.linkedin}/>
+      </div>
+    )
+  }
 }
 
 export default About;
